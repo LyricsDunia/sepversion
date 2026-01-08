ed89b462dc8c7e36d9f46e24d98928c5b578e20b
LyricsDunia <lyricsdunia9@gmail.com>
2026-01-08 15:46:15 +0000

    adjusted header.js and for logo changed to ../public/index.html

--- sepversion/src/App.js
+++ sepversion/src/App.js
@@ -108,7 +108,7 @@ app.get('/api/price-history/:productId', async (req, res) => {
 
 // Serve the main HTML file
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, "..", 'public', 'index.html'));
 });
 
 // Start server
